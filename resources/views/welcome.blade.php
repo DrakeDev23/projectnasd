df8b51db07eacfe1418e865bfa61cb43d58e891f
DrakeDev23 <maccogoth@gmail.com>
2026-09-10 22:11:24 +0800

    added nav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,8 @@
             <nav>
                 <ul>
                     <li><a href="">home</a></li>
+                    <li><a href="">about</a></li>
+                    <li><a href="">contact</a></li>
                 </ul>
             </nav>
         </div>
